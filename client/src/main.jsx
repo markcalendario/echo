c268864ac386463f7f0c073b6ef2d2abd501b17b
markcalendario <markcalendario@gmail.com>
2025-03-18 15:01:22 +0800

    chore: add alias

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
+import App from "@/App.jsx";
+import "@/styles/main.scss";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./styles/main.scss";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
