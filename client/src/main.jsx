1e6f1a8e0fd0c91f48ff58c57e8275fd5c352464
markcalendario <markcalendario@gmail.com>
2025-03-19 16:52:09 +0800

    feat: remove double render

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,6 @@
 import App from "@/App.jsx";
 import "@/fontawesome/css/all.min.css";
 import "@/styles/main.scss";
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.getElementById("root")).render(<App />);
