daebeae60406114584b29ae85baa9ab23e0213e9
markcalendario <markcalendario@gmail.com>
2025-03-20 22:32:45 +0800

    feat: implement username api on navbar

--- client/src/layouts/Dashboard/Navbar/Navbar.jsx
+++ client/src/layouts/Dashboard/Navbar/Navbar.jsx
@@ -1,6 +1,39 @@
+import fetchAPI from "@/utils/fetch.js";
+import { useEffect, useState } from "react";
 import styles from "./Navbar.module.scss";
 
 export default function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <LeftContent />
+          <RightContent />
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+function LeftContent() {
+  return (
+    <div className={styles.left}>
+      <a
+        href="/"
+        className={styles.logo}>
+        <img
+          src="/assets/logo/primary.svg"
+          alt="Echo Logo"
+        />
+        <span>Echo</span>
+      </a>
+    </div>
+  );
+}
+
+function RightContent() {
+  const [username, setUsername] = useState("");
+
   const handleToggleSidebar = () => {
     const sidebar = document.getElementById("sidebar");
     const display = sidebar.style.display || null;
@@ -12,38 +45,34 @@ export default function Navbar() {
     }
   };
 
+  const fetchUsername = async () => {
+    const config = { credentials: "include" };
+    const response = await fetchAPI("/users/username", config);
+    setUsername(response.username);
+  };
+
+  useEffect(() => {
+    fetchUsername();
+  }, []);
+
+  if (!username) return;
+
   return (
-    <nav className={styles.navbar}>
-      <div className={styles.container}>
-        <div className={styles.wrapper}>
-          <a
-            href="/"
-            className={styles.logo}>
-            <img
-              src="/assets/logo/primary.svg"
-              alt="Echo Logo"
-            />
-            <span>Echo</span>
-          </a>
-
-          <div className={styles.right}>
-            <div className={styles.user}>
-              <img
-                src="/assets/images/misc/default-profile.jpg"
-                alt="User Avatar"
-              />
-              <a>kenesu00</a>
-            </div>
-
-            <button
-              aria-label="Toggle menu"
-              className={styles.burger}
-              onClick={handleToggleSidebar}>
-              <i className="fas fa-bars" />
-            </button>
-          </div>
-        </div>
+    <div className={styles.right}>
+      <div className={styles.user}>
+        <img
+          src="/assets/images/misc/default-profile.jpg"
+          alt="User Avatar"
+        />
+        <a>{username}</a>
       </div>
-    </nav>
+
+      <button
+        aria-label="Toggle menu"
+        className={styles.burger}
+        onClick={handleToggleSidebar}>
+        <i className="fas fa-bars" />
+      </button>
+    </div>
   );
 }
