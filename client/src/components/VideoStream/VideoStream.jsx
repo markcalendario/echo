9df1c5de51c5c91acb7aa03fbeb43319cb35caa7
markcalendario <markcalendario@gmail.com>
2025-03-24 14:26:51 +0800

    feat: make status and hls as parameters

--- client/src/components/VideoStream/VideoStream.jsx
+++ client/src/components/VideoStream/VideoStream.jsx
@@ -1,33 +1,15 @@
 import useStreamer from "@/hooks/useStreamer.jsx";
-import socket from "@/utils/socket.io.js";
-import { useEffect, useRef, useState } from "react";
+import { useRef } from "react";
 import { Controls } from "./Controls/Controls.jsx";
 import StreamStatusTab from "./StreamStatusTab/StreamStatusTab.jsx";
 import styles from "./VideoStream.module.scss";
 
-export default function VideoStream({ className, streamerID }) {
-  const [status, setCurrentStatus] = useState("OFFLINE");
-  const [hlsPlayback, setHLSPlayback] = useState("");
-
+export default function VideoStream({ className, status, hlsPlayback }) {
   const videoRef = useRef(null);
   const containerRef = useRef(null);
   useStreamer(videoRef, hlsPlayback);
 
   const classes = [className, styles.videoStream].filter(Boolean).join(" ");
-
-  const handleOnStreamEvent = (streamData) => {
-    setCurrentStatus(streamData.status);
-    setHLSPlayback(streamData.hlsPlayback);
-  };
-
-  useEffect(() => {
-    socket.emit("get-stream-data", streamerID);
-    socket.on("get-stream-data", (streamData) =>
-      handleOnStreamEvent(streamData)
-    );
-
-    return () => socket.off("stream");
-  }, [status, hlsPlayback]);
 
   return (
     <div
--- client/src/pages/Me/Me.jsx
+++ client/src/pages/Me/Me.jsx
@@ -5,6 +5,7 @@ import Content from "@/layouts/Dashboard/Content/Content.jsx";
 import Dashboard from "@/layouts/Dashboard/Dashboard.jsx";
 import SignedInRoute from "@/route-protections/SignedInRoute.jsx";
 import fetchAPI from "@/utils/fetch.js";
+import socket from "@/utils/socket.io.js";
 import { showErrorToast, showSuccessToast } from "@/utils/toast.js";
 import { Fragment, useEffect, useState } from "react";
 import styles from "./Me.module.scss";
@@ -21,6 +22,10 @@ export default function Me() {
 
 function MyStream() {
   const [userID, setUserID] = useState(null);
+  const [streamData, setStreamData] = useState({
+    status: null,
+    hlsPlayback: null
+  });
 
   const fetchMyStream = async () => {
     const config = { method: "GET", credentials: "include" };
@@ -33,14 +38,22 @@ function MyStream() {
     fetchMyStream();
   }, []);
 
+  useEffect(() => {
+    socket.emit("get-stream-data", userID);
+    socket.on("get-stream-data", (streamData) => setStreamData(streamData));
+
+    return () => socket.off("stream");
+  }, [userID]);
+
   if (userID === null) return;
 
   return (
     <Fragment>
       <Content className={styles.myStream}>
         <VideoStream
-          streamerID={userID}
+          status={streamData.status}
           className={styles.videoStream}
+          hlsPlayback={streamData.hlsPlayback}
         />
       </Content>
 
